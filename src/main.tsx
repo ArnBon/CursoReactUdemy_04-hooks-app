bb77458cf20283cc4e775ec4b44105be289fbdc3
ArnBon <bonillaarnaldo@gmail.com>
2026-09-19 12:06:38 -0400

    clase 153 useOptimistic Preparacion del ejercicio

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { InstagromApp } from './07-useOptimistic/InstagromApp'
 // import { MemoHook } from './06-memos/MemoHook'
-import { MemoCounter } from './06-memos/MemoCounter'
+// import { MemoCounter } from './06-memos/MemoCounter'
 // import { FocusScreen } from './04-useRef/FocusScreen'
 // import { TaskApp } from './05-useReducer/TaskApp'
 // import { ScrambleWords } from './05-useReducer/ScrambleWords'
@@ -24,6 +25,7 @@ createRoot(document.getElementById('root')!).render(
     {/* <TaskApp></TaskApp> */}
     {/* <ScrambleWords></ScrambleWords> */}
     {/* <MemoHook></MemoHook> */}
-    <MemoCounter></MemoCounter>
+    {/* <MemoCounter></MemoCounter> */}
+    <InstagromApp></InstagromApp>
   </StrictMode>,
 )
